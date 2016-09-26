62f13dfea1c4cfc6ae044af176bd130bc86fca6e
ekougs <ekougs@gmail.com>
2016-09-26 13:23:40 +0000

    direct_download_url_and_pdf_js_thumbnail: direct download url in meta for /api/v1/documents

--- sira/urls.py
+++ sira/urls.py
@@ -10,6 +10,7 @@ from django.contrib import admin
 from wagtail.contrib.wagtailapi.endpoints import PagesAPIEndpoint, ImagesAPIEndpoint, DocumentsAPIEndpoint
 from wagtail.contrib.wagtailapi.router import WagtailAPIRouter
 from wagtail.contrib.wagtailapi.serializers import DocumentMetaField, BaseSerializer
+from wagtail.contrib.wagtailapi.utils import get_full_url
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
@@ -20,8 +21,13 @@ from sira import views
 class DocumentExtraFieldsMetaField(DocumentMetaField):
     def to_representation(self, document):
         representation = super(DocumentExtraFieldsMetaField, self).to_representation(document)
-        representation['extension'] = document.file.name.split(os.extsep)[-1]
+        representation['extension'] = DocumentExtraFieldsMetaField.get_document_file_ext(document)
+        representation['download_url'] = get_full_url(self.context['request'], document.url)
         return representation
+
+    @staticmethod
+    def get_document_file_ext(document):
+        return document.file.name.split(os.extsep)[-1]
 
 
 class DocumentsExtraFieldsSerializer(BaseSerializer):
